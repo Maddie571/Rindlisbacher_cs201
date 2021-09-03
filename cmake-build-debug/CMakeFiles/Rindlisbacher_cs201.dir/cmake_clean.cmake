a9221463b2552e597c64339ddfab29b55115aaf3
Matt Rindlisbacher <mprindlisbacher@alaska.edu>
2021-09-03 09:52:45 -0800

    Lab 1 with the extra task

---
+++ cmake-build-debug/CMakeFiles/Rindlisbacher_cs201.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rindlisbacher_cs201.dir/Hello_World.cpp.obj"
+  "Rindlisbacher_cs201.exe"
+  "Rindlisbacher_cs201.exe.manifest"
+  "Rindlisbacher_cs201.pdb"
+  "libRindlisbacher_cs201.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Rindlisbacher_cs201.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
