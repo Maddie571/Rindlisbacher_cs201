cd90a7f2b7cd5918223ae93224d878e37621261b
Matt Rindlisbacher <mprindlisbacher@alaska.edu>
2021-09-08 10:06:44 -0800

    Lab 4 started

---
+++ Fizz.cpp
@@ -0,0 +1,15 @@
+//
+// Created by mprindlisbacher on 9/8/2021.
+//
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main(){
+    for(int i = 1; i <= 100; i++){
+        cout << i;
+    }
+    return 4;
+}
+
