c503ce3eb87c0dbc6443f0f5994f46b6c7d6421a
Matt Rindlisbacher <mprindlisbacher@alaska.edu>
2021-09-08 09:43:03 -0800

    Lab 2 with the extras added

---
+++ BoxesBoxesBoxes.cpp
@@ -0,0 +1,18 @@
+//
+// Created by mprindlisbacher on 9/8/2021.
+//
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main(){
+    int n = 0;
+    while (n < 20){
+        n++;
+        cout << "\n" << n;
+    }
+    return n;
+
+}
+
