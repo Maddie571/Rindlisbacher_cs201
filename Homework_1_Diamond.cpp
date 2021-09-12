8e639a768fff2f0481e7083b08ad3068f3381007
maddie571 <mattrind2@gmail.com>
2021-09-11 17:53:17 -0800

    Homework 1 diamonds, top half working

--- Homework_1_Diamond.cpp
+++ Homework_1_Diamond.cpp
@@ -4,18 +4,25 @@
 
 #include <iostream>
 using std::cout;
+using std::cin;
 using std::endl;
 
 int main(){
-    int n;
+    int Height;
+    int PoundSigns = 1;
     cout << "Pick a Number:";
-    cin >> n; //get the number of lines
-    for (int i = 1; i <= n; i++){
-        for (int j = n-i; j >= i; j--;){
+    cin >> Height; //get the number of lines
+    for (int Line = Height; Line > 0; Line--){ //top half of diamond
+        for (int Spaces = Line; Spaces >= 0; Spaces--){ //the number of spaces needed is the same as the iputed number minus whatever line its currently on
             cout << " ";
         }
-        cout << "#" << endl;
+        for (int i = 0; i < PoundSigns; i++) {
+            cout << "#";
+        }
+        PoundSigns += 2; //needed pound signs increases by 2 instead of 1 per line
+        cout << endl;
     }
+
 
     return 2;
 }
