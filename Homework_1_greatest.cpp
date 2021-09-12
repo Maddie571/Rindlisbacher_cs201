f0870e56a827a9966fcba718e2717f420b183182
maddie571 <mattrind2@gmail.com>
2021-09-11 21:23:34 -0800

    Homework 1 greatest completed

--- Homework_1_greatest.cpp
+++ Homework_1_greatest.cpp
@@ -13,19 +13,20 @@ int main(){
 int greatest = 0;
 int count = 0;
 int n = 1;
-cout << "begin entering numbers";
+cout << "begin entering numbers, enter 0 to stop \n I'll return your highest input";
 while (n > 0){
     cin >> n;
     if (n > greatest){
         greatest = n;
     }
     count ++;
+    cout << "Next Number:";
 }
 if (count > 0) {
     return greatest;
 } else {
     cout << "please input at least one number greater than 0";
-    return null;
+    return 0;
 }
 }
 
