b0d2ad40b76efdde0b3fa2fb107c955c919a0ece
maddie571 <mattrind2@gmail.com>
2021-09-11 21:14:28 -0800

    Homework 1 greatest written; untested

--- Homework_1_greatest.cpp
+++ Homework_1_greatest.cpp
@@ -2,14 +2,31 @@
 // Created by photo on 9/11/2021.
 //
 
+//input numbers until something less than 1 has been inputted, and return with the highest number
+// just run through an if statement after each inputted number, need a safe for if the first number is below 1
 #include <iostream>
 using std::cout;
 using std::cin;
 using std::endl;
 
 int main(){
-
-    return 2;
+int greatest = 0;
+int count = 0;
+int n = 1;
+cout << "begin entering numbers";
+while (n > 0){
+    cin >> n;
+    if (n > greatest){
+        greatest = n;
+    }
+    count ++;
+}
+if (count > 0) {
+    return greatest;
+} else {
+    cout << "please input at least one number greater than 0";
+    return null;
+}
 }
 
 
