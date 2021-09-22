69fcf336dd7c48b0ba7e010e2d0e2b7de5fa4ce9
maddie571 <mattrind2@gmail.com>
2021-09-21 19:31:12 -0800

    Lab 6 main done, needs extras.

--- Functions.cpp
+++ Functions.cpp
@@ -12,14 +12,17 @@ using std::string;
 using std::endl;
 
 int main(){
+    cout << "Pick a word, any word, but only 1 word";
     string s = "";
     cin >> s;
     DoThing(s);
-    return 2;
+    return 6;
 }
 
 void DoThing(string Word) {
+    cout << "Your word is:";
     cout << Word << endl;
     cout << Word.size();
+    cout << " Letters long!";
 }
 
