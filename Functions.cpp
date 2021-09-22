c364b5f3f02e239cb6f26e88df9acaa11de89a03
maddie571 <mattrind2@gmail.com>
2021-09-21 19:08:15 -0800

    Lab 6 started

---
+++ Functions.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Matt RIndlisbacher on 9/21/2021.
+//
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::string;
+
+int main(){
+    DoThing (cin >> string s);
+    return 2;
+}
+
+void DoThing (std::string Word){
+
+}
+
