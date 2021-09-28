1ce7ac572e4968ebcdd9184618e7f71c6ca370dc
maddie571 <mattrind2@gmail.com>
2021-09-27 16:51:49 -0800

    Lab 7 started

--- Functions.cpp
+++ Functions.cpp
@@ -33,3 +33,5 @@ void DoThing(string Word) {
     }
 }
 
+
+
---
+++ Lab7.cpp
@@ -0,0 +1,16 @@
+//
+// Created by photo on 9/27/2021.
+//
+
+#include "intio.cpp"
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(){
+    cout << "please type an integer:";
+    int x = getInt();
+    return x;
+}
+
