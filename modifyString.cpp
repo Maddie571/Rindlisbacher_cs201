841bec11d06ce37aa9449fa15ad26c1d1aa6a698
maddie571 <mattrind2@gmail.com>
2021-09-27 18:40:13 -0800

    Lab 9 started

---
+++ modifyString.cpp
@@ -0,0 +1,16 @@
+//
+// Created by photo on 9/27/2021.
+//
+
+#include "modifyString.hpp"
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main()
+{
+    return 0;
+}
+
