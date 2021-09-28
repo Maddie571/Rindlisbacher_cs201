624786f14480b14407a7461865832bb76ac37f9e
maddie571 <mattrind2@gmail.com>
2021-09-27 18:21:35 -0800

    Lab 8 started

---
+++ lab8.cpp
@@ -0,0 +1,20 @@
+//
+// Created by photo on 9/27/2021.
+//
+
+#include "lab8.hpp"
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int doInput(){
+return getInt();
+}
+int compute(){
+int x = getInt();
+return x*x;
+}
+
+
+
