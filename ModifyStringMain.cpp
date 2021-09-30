2ef533b69eab1f38a6567482fbe47c7230546cec
maddie571 <mattrind2@gmail.com>
2021-09-29 17:59:04 -0800

    Lab 9 done, duplicates the string a number of times equal to the inputted number

--- ModifyStringMain.cpp
+++ ModifyStringMain.cpp
@@ -12,12 +12,15 @@ int main()
 {
     string s;
     int duplicates;
+    cout << "How many times would you like to duplicate your word?";
     cin >> duplicates;
     modifyString(s, duplicates);
     cout << s;
+    cout << "\nHow many times would you like to duplicate your next word?";
     cin >> duplicates;
     modifyString(s, duplicates);
     cout << s;
+    cout << "\nHow many times would you like to duplicate your final word?";
     cin >> duplicates;
     modifyString(s, duplicates);
     cout << s;
